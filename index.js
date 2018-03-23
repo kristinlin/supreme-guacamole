c29974af1745d75596d8ec8f2cfb4a8f81047cc0
kristinlin <klin11@stuy.edu>
2018-03-23 14:00:06 -0400

    cw

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var yr00 = document.getElementById("00");
+var yr20 = document.getElementById("20");
+
+//makebars will create the divs with the correct style
+// d3.selectAll("div")
+// .bind(data)
+// .enter()
+
+
+//scale_transition(time)
+//.transition
+//.duration(time)
+//.style("width", function(d)...
+
+var toggle00 = function(e) {
+
+}
+
+var toggle20 = function(e) {
+
+}
+
+yr00.addEventListener("click", toggle00);
+yr20.addEventListener("click", toggle20);
